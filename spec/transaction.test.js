f49c22b74c9841dafcc8adb9d65f31dc0673d1f9
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-11-15 18:06:57 +0000

    passing test on transaction of account opening :)

---
+++ spec/transaction.test.js
@@ -0,0 +1,11 @@
+const Transaction = require('../lib/transaction')
+
+describe('Transaction', () => {
+    it('makes a deposit', () => {
+        const myAccount = {
+            newBalance: (total) => {},
+            getBalance: () => {1000}
+        };
+    });
+
+});

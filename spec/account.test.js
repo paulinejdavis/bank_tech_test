b64c58fc7f81fe208c5c54f795a157b61c0a40e8
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-11-15 17:43:19 +0000

    passing test on credit tranaction of £2k

--- spec/account.test.js
+++ spec/account.test.js
@@ -3,7 +3,13 @@ const Account = require('../lib/account')
 describe('Account', () => {
     it ('returns the balance of the account', () => {
         const account = new Account;
-        expect(account.getBalance()).toBe(0);
+        expect(account.getBalance()).toEqual(1000);
+    });
+
+    it ('returns the new balance after a credit of £1000', () => {
+        const account = new Account;
+        account.newBalance(2000);
+        expect(account.getBalance()).toBe(3000);
     });
 });
 
